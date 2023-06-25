3902d97c9d3104f66f56328a3cdafb0b1756da6b
Roe Josept L. Mejia <74046150+iamroejosept@users.noreply.github.com>
2023-06-25 09:10:30 -0600

    06/25/2023

--- movieManiaDatabase/Rentals.sql
+++ movieManiaDatabase/Rentals.sql
@@ -6,6 +6,6 @@
     [rental_date] DATE NULL, 
     [return_date] DATE NULL, 
     [status] VARCHAR(MAX) NULL,
-    FOREIGN KEY ([customer_id]) REFERENCES [dbo].[Customers]([customer_id]),
-    FOREIGN KEY ([movie_id]) REFERENCES [dbo].[Movies]([movie_id])
+    FOREIGN KEY ([customer_id]) REFERENCES [dbo].[Customers]([customer_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([movie_id]) REFERENCES [dbo].[Movies]([movie_id]) ON DELETE CASCADE
 )
