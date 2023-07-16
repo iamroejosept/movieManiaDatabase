04aad80cb97e98769a1c1fad865cecd03709b848
Roe Josept L. Mejia <74046150+iamroejosept@users.noreply.github.com>
2023-07-16 07:08:22 -0600

    07/16/2023

---
+++ movieManiaDatabase/Script.PreDeployment/PreDeploymentScript.sql
@@ -0,0 +1,11 @@
+﻿DECLARE @BackupPath NVARCHAR(500);
+DECLARE @BackupFileName NVARCHAR(500);
+
+-- Specify the backup path
+SET @BackupPath = 'C:\DB Backup\';
+
+-- Generate the backup filename with the current date
+SET @BackupFileName = @BackupPath + 'movieMania_' + CONVERT(NVARCHAR(20), GETDATE(), 112) + '.bak';
+
+-- Perform the backup
+BACKUP DATABASE movieManiaDatabase TO DISK = @BackupFileName;
